1d774d9d2fa36f31046eeda242fd9cc788161101
Nitesh Sahu <121674879+technicalnitesh@users.noreply.github.com>
2026-07-30 11:27:16 +0530

    media files url add

--- memory_tales/settings.py
+++ memory_tales/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_5_CONFIGS = {
 
