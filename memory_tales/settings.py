e559875e567e3897adf0cb84a73bf20d9dd4c548
Nitesh Sahu <121674879+technicalnitesh@users.noreply.github.com>
2026-07-30 11:37:29 +0530

    static files

--- memory_tales/settings.py
+++ memory_tales/settings.py
@@ -120,9 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
